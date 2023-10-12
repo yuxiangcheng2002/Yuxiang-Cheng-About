d9a9580d5b625fb57c84898eb7fa56827b2bd7bc
Yuxiang Cheng <yuxiangcheng2002@gmail.com>
2023-10-12 08:04:48 -0400

    v1.1

--- custom.js
+++ custom.js
@@ -1,4 +1,4 @@
-var myProjects = new Array('./Projects/1.jpg','./Projects/2.jpg','./Projects/3.jpg','./Projects/4.jpg');
+var myProjects = new Array('./Projects/1.JPG','./Projects/2.jpg','./Projects/3.jpg','./Projects/4.jpg');
 var myPortraits = new Array('./Portraits/1.jpg','./Portraits/2.jpg','./Portraits/3.jpg','./Portraits/4.jpg');
 
 window.onload = function(){
